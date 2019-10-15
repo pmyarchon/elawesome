ea0b8d67993b99f80d934991fd886431be9b54ac
Maxim Pushkar <archon@mail.ru>
2019-10-15 04:06:57 +0300

    Add utility functions to parse content from h4cc/awesome-elixir

---
+++ lib/elawesome/utils_parse.ex
@@ -0,0 +1,45 @@
+defmodule Utils.Parse do
+  @h4cc_github "https://github.com/h4cc/awesome-elixir"
+
+  def repos() do
+    {:ok, html} = Utils.get_contents(@h4cc_github)
+    blocks = :lists.dropwhile(fn
+       ({"h2", _, _}) -> false
+       ({"h2", _, _, _}) -> false
+       _ -> true
+    end, Floki.find(html, ".Box-body > article > *"))
+    parse_repos(blocks)
+  end
+
+  # Parse repositories
+  defp parse_repos(html_tree) do
+    for html_subtree <- split_repos(html_tree) do
+      %RepoGroup{
+        name: Floki.find(html_subtree, "h2") |> Floki.filter_out("a") |> Floki.text([deep: false]),
+        description: Floki.find(html_subtree, "p > em") |> Floki.text([deep: false]),
+        url: Floki.find(html_subtree, "h2 > a") |> Floki.attribute("href"),
+        items: Floki.find(html_subtree, "ul > li") |> Enum.map(&parse_repo/1)
+      }
+    end
+  end
+
+  # Split HTML tree structure into repository groups (group title / subtitle / repositories)
+  defp split_repos(html_tree), do: split_repos(html_tree, [])
+
+  defp split_repos([], acc), do: Enum.reverse(acc)
+  defp split_repos([{"h2", _, _} = a, {"p", _, _} = b, {"ul", _, _} = c | tail], acc), do: split_repos(tail, [[a, b, c] | acc])
+  defp split_repos([_h | tail], acc), do: split_repos(tail, acc)
+
+  # Parse repository
+  defp parse_repo(html_subtree) do
+    link = Floki.find(html_subtree, "a:first-child")
+    %Repo{
+      name: link |> Floki.text([deep: false]),
+      description: Floki.filter_out(html_subtree, "a:first-child") |> Floki.find("li") |> contents() |> Floki.raw_html() |> String.trim_leading(" - "),
+      url: link |> Floki.attribute("href"),
+    }
+  end
+
+  # HTML tree element contents
+  defp contents([{_tag, _attrs, content} | _]), do: content
+end
