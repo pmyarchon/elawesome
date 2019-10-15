a9158682b9346d0f9d5fa95adb159f649ce78685
Maxim Pushkar <archon@mail.ru>
2019-10-15 23:05:57 +0300

    Add RepoExtInfo struct

--- lib/elawesome/structs.ex
+++ lib/elawesome/structs.ex
@@ -1,8 +1,11 @@
-defmodule Repo do
-  defstruct [:name, :description, :url, :stars, :last_commit]
-end
-
 defmodule RepoGroup do
   defstruct [:name, :description, :url, :items]
 end
 
+defmodule Repo do
+  defstruct [:name, :description, :url, :stars, :last_commit, :days_ago]
+end
+
+defmodule RepoExtInfo do
+  defstruct [:url, :stars, :last_commit, :days_ago]
+end
