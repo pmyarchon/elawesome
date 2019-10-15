5ccab5b1aa960128c925638b3021360ee952b75a
Maxim Pushkar <archon@mail.ru>
2019-10-15 04:06:32 +0300

    Add simple utility function to get page content via HTTP GET request done by httpc

--- lib/elawesome/utils.ex
+++ lib/elawesome/utils.ex
@@ -2,4 +2,11 @@ defmodule Utils do
   def timestamp do
     :os.system_time(:seconds)
   end
+
+  def get_contents(url) do
+    case :httpc.request(:get, {to_charlist(url), []}, [], [{:body_format, :binary}]) do
+      {:ok, {{_version, 200, _status_text}, _headers, body}} -> {:ok, body}
+      _ -> nil
+    end
+  end
 end
