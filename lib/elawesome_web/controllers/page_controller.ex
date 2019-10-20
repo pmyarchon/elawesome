e4b08d331e84783806f50b1e52d8553e4f17eef8
Maxim Pushkar <archon@mail.ru>
2019-10-21 02:11:23 +0300

    Filter and display repositories

--- lib/elawesome_web/controllers/page_controller.ex
+++ lib/elawesome_web/controllers/page_controller.ex
@@ -1,7 +1,31 @@
 defmodule ElawesomeWeb.PageController do
   use ElawesomeWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  def index(conn, params) do
+    # Get minimal stars
+    min_stars = Utils.maybe_int(params["min_stars"], 0)
+
+    # Filter repositories
+    status = Elawesome.Storage.status()
+
+    items = if status === :idle do
+      repos = Elawesome.Storage.filter_repos(min_stars)
+      group_keys = Enum.reduce(repos, MapSet.new(), &(MapSet.put(&2, &1.group)))
+      Elawesome.Storage.groups()
+        |> Enum.filter(&(MapSet.member?(group_keys, &1.name)))
+        |> Enum.map(&(%{group: &1, repos: Enum.filter(repos, fn el -> el.group === &1.name end)}))
+        |> Enum.filter(&(&1[:items] !== []))
+    else
+      []
+    end
+
+    render conn, "index.html",
+      min_stars: min_stars,
+      is_warm_up: Elawesome.Storage.warm_up?,
+      status: status,
+      num_total: Elawesome.Storage.total,
+      num_processed: Elawesome.Storage.processed,
+      num_failed: Elawesome.Storage.failed,
+      items: items
   end
 end
--- lib/elawesome_web/views/page_view.ex
+++ lib/elawesome_web/views/page_view.ex
@@ -1,13 +1,5 @@
 defmodule ElawesomeWeb.PageView do
   use ElawesomeWeb, :view
 
-  def libs(conn) do
-    # min_stars = case conn.params["min_stars"] do
-    #   stars when is_binary(stars) -> :erlang.binary_to_integer(stars)
-    #   _ -> 0
-    # end
-    # "Show repos with at least of #{min_stars} stars"
-
-    Utils.Parse.repos()
-  end
+  def to_anchor(url), do: String.trim_leading(url, "#")
 end
