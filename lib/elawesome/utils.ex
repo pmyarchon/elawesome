35a81d49aba7b67de4524cda44365aef29fe27f3
Maxim Pushkar <archon@mail.ru>
2019-10-21 02:09:12 +0300

    Add maybe_int/2 and prettify get_contents/1 function in Utils

--- lib/elawesome/utils.ex
+++ lib/elawesome/utils.ex
@@ -5,9 +5,12 @@ defmodule Utils do
 
   def get_contents(url) do
     case :httpc.request(:get, {to_charlist(url), []}, [], [{:body_format, :binary}]) do
-      # {:ok, {{_version, 200, _status_text}, _headers, body}} -> {:ok, body}
       {:ok, {{_version, _, _status_text}, _headers, body}} -> {:ok, body}
       _ -> nil
     end
   end
+
+  def maybe_int(nil, default), do: default
+  def maybe_int(value, _) when is_integer(value), do: value
+  def maybe_int(value, _) when is_binary(value), do: :erlang.binary_to_integer(value)
 end
